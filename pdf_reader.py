d99208aadecb4d486f613df2cccc25e144318ebe
Samira-lara <160476100+Samira-lara@users.noreply.github.com>
2024-03-22 21:06:14 +0600

    Add files via upload

---
+++ pdf_reader.py
@@ -0,0 +1,16 @@
+import PyPDF2
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with open(pdf_path, "rb") as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        num_pages = len(pdf_reader.pages)
+        for page_number in range(num_pages):
+            page = pdf_reader.pages[page_number]
+            text += page.extract_text()
+    return text
+"""
+pdf_path = "any.pdf"  
+extracted_text = extract_text_from_pdf(pdf_path)
+print(extracted_text)
+"""
